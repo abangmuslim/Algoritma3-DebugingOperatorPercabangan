534eaa1bb5773eca27eb6f037544af9abefc6044
abangmuslim <abangmuslimtamiang@gmail.com>
2024-08-29 09:31:11 +0700

    membuat print r

---
+++ 01 print r array.php
@@ -0,0 +1,9 @@
+<?php
+$nama = "Budi";
+print_r($nama);
+print "<p>";
+$buah = array("apel", "pisang", "ceri", "mangga");
+print_r ($buah);
+print "<p>";
+printf ($buah [2]);
+
---
+++ 02 print r return value.php
@@ -0,0 +1,8 @@
+<?php
+$buah = array("apel", "pisang", "ceri");
+$hasil1 = print_r($buah, true);
+$hasil2 = print_r($buah, false);
+
+echo $hasil1;
+print "<p>";
+echo $hasil2;
