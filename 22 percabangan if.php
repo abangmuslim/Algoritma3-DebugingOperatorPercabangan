552e60736a48cffd3168819aff8ad83f9a628b78
abangmuslim <abangmuslimtamiang@gmail.com>
2024-08-29 21:27:52 +0700

    percabangan if dan if else

---
+++ 22 percabangan if.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$total_belanja = 150000;
+
+if($total_belanja > 100000){
+   echo "karena belanjaan anda sudah melebihi $total_belanja , maka Anda akan dapat DISKON 75%!";
+}
+
+$siswa = "rajin";
+if($siswa = "rajin"){
+    echo "<hr>jika rajin akan NAIK KELAS!";
+}
